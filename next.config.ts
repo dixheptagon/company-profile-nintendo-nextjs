bab4d4b01471a3d8948b9da805c8688aae484013
dixheptagon <forentino006@gmai.com>
2025-08-07 02:07:47 +0700

    add config ignore  eslint during build

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["randomuser.me", "cdn-site.gojek.com", "assets.nintendo.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
