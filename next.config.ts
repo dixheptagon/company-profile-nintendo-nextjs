65c34262113f5158e30c2a6a2400c81a7786be9b
dixheptagon <forentino006@gmai.com>
2025-08-07 14:47:27 +0700

    fix config for image randomuser

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,6 @@
 const nextConfig = {
   images: {
     domains: ["randomuser.me", "cdn-site.gojek.com", "assets.nintendo.com"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "randomuser.me",
-        pathname: "/api/**", // penting: sesuaikan path
-      },
-    ],
   },
   eslint: {
     ignoreDuringBuilds: true,
